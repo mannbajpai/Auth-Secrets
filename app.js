de5d084775cfb188334c78c0db0848bb777e8461
mannbajpai <mannbajpai.grs@gmail.com>
2023-01-14 19:08:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err,foundUser) => {
+        if(err){
+            console.log(err)
+        } else {
+            if (foundUser){
+                if (foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+})
+
+app.listen(3000, () => {
+  console.log("Server Running on Port 3000");
+});
